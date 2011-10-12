7b2e16e57c21195687b4bd6b2fb4820220714b50
Andor <zandor86@gmail.com>
2011-10-12 19:36:27 +0200

    RCBuilderTest added

---
+++ pc-remote-for-android/pc-remote-test/src/hu/za/pc_remote/ui/RCBuilder/RCBuilderTest.java
@@ -0,0 +1,43 @@
+package hu.za.pc_remote.ui.RCBuilder;
+
+import android.os.Environment;
+import android.test.ActivityInstrumentationTestCase2;
+import android.widget.TextView;
+import hu.za.pc_remote.HelloActivity;
+import hu.za.pc_remote.R;
+
+import java.io.*;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: Andor
+ * Date: 10/11/11
+ * Time: 11:31 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class RCBuilderTest extends ActivityInstrumentationTestCase2<RCBuilder> {
+
+    private RCBuilder mActivity;
+
+    public RCBuilderTest() {
+        super("hu.za.pc_remote.ui.RCBuilder", RCBuilder.class);
+    }
+
+    @Override
+    protected void setUp() throws Exception {
+        super.setUp();
+        mActivity = this.getActivity();
+    }
+
+    public void testPreconditions() {
+        mActivity.getRCDirectories();
+
+        try {
+            FileReader fr = new FileReader(Environment.getDataDirectory().getPath() + File.pathSeparatorChar + "pcremote" + File.pathSeparatorChar + "file");
+            BufferedReader br = new BufferedReader(fr);
+            assertTrue(br.readLine().startsWith("HelloWorld"));
+        } catch (IOException e) {
+            e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+        }
+    }
+}
